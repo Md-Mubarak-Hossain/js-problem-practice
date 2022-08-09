a36d3193a05810a178d6e53bdc587edeb764188f
Md-Mubarak-Hossain <hossainmubarak477@gmail.com>
2022-08-09 21:07:45 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,15 @@
+
+// string type variable
+var stringString = 'Parvej ajad gumiye gese;'
+console.log(typeof (stringString));
+
+// number type variable
+let numberNumber = 23;
+console.log(typeof (numberNumber));
+// boolean type variable
+booleanBool = true;
+console.log(typeof (booleanBool));
+
+
+
+
